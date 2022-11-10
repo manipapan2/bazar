16e13679675327430fdaf0d40306941a4869e7c8
manipapan2 <105167359+manipapan2@users.noreply.github.com>
2022-11-10 11:46:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let mh = t1.offsetHeight
 
 function maxheight(){
     document.getElementById("p1").style.maxHeight=document.getElementById("tozih1").offsetHeight
+    
+    document.getElementById("p2").style.maxHeight=document.getElementById("tozih2").offsetHeight
+    
+    document.getElementById("p3").style.maxHeight=document.getElementById("tozih3").offsetHeight
+    
+    document.getElementById("p4").style.maxHeight=document.getElementById("tozih4").offsetHeight
 }
 
 maxheight()
